12094d66bf4cec04d0e045126b1a5053549405c2
BEAR\speed <speeduno@nate.com>
2025-04-28 02:20:10 +0900

    250428 멀티게임을 위한 추가

---
+++ ProjectDD/Source/ProjectDD/GameMode/DDAdvanceGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "DDAdvanceGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTDD_API ADDAdvanceGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
